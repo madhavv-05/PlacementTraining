8d1aa3079d0add390777daf7cad0bd6601b4f207
Madhav Gupta <madhav87308@gmail.com>
2025-02-05 11:34:47 +0530

    113405022025

---
+++ hello.py
@@ -0,0 +1,36 @@
+
+import math
+"""
+name=input("Enter your name: ")
+
+print(name)
+
+num1=int(input("Enter number 1: "))
+num2=int(input("Enter number 2: "))
+
+sum=num1+num2
+
+
+print("sum of ",num1," and ",num2," is: ",sum)
+
+
+l=int(input("enter length of triangle:  "))
+b=int(input("eter base of triangle:  "))
+
+h=((l**2+b**2)**0.5)
+
+print("hypotaneuse of triangle is: ",h)
+
+
++----------+
+|                      |
+|                      |
+|                      |
+|                      |
+|                      |
++----------+
+"""
+print("+----------+")
+print("|          |\n|          |\n|          |\n|          |\n|          |")
+
+print("+----------+")
