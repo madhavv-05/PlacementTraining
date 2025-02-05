e35c56fc7f043ae697cb8dea479ec0dba1090530
Madhav Gupta <madhav87308@gmail.com>
2025-02-05 19:13:36 +0530

    191305022025

--- hello.py
+++ hello.py
@@ -29,8 +29,62 @@ print("hypotaneuse of triangle is: ",h)
 |                      |
 |                      |
 +----------+
-"""
+
 print("+----------+")
 print("|          |\n|          |\n|          |\n|          |\n|          |")
 
 print("+----------+")
+
+
+var=0
+print(var==0)
+
+var=1
+print(var==0)
+
+
+
+
+sheep_counter = 10
+
+
+if sheep_counter >=120:
+    print("make_a_bed")
+    print("take_a_shower")
+    print("sleep_and_dream")
+print("feed the sheens")
+
+
+sheep_counter = 130
+weather_is_good = True
+if sheep_counter >=120:
+    if weather_is_good:
+        print("make_a_bed")
+        print("take_a_shower")
+        print("sleep_and_dream")
+    else:
+        print("I cannot go out")
+else:
+    print("Not able to count sheep")
+
+number1 = int(input("Enter the first number: "))
+number2 = int(input("Enter the second number: "))
+
+number1=int(input("Enter number 1: "))
+number2=int(input("Enter number 2: "))
+if number1 > number2: arger_number = number1
+else:larger_number = number2
+
+print("The larger number is:", larger_number)
+"""
+
+number1=int(input("Enter number 1: "))
+number2=int(input("Enter number 2: "))
+number3=int(input("Enter number 3: "))
+
+largest=number1
+
+if(number2>largest): largest=number2
+if(number3>largest): largest=number3
+
+print("largest number is: ",largest)
